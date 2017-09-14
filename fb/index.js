fe407d381fcb57baccd08a659ea86eab3866fe7d
cuipengfei1 <cuipengfei1@jd.com>
2017-09-14 15:01:23 +0800

    facebook二次分享

--- fb/index.js
+++ fb/index.js
@@ -1,9 +1,22 @@
 window.onload = function(){
+    facebookSDK.init();
     var param=pageObj.getParameter("pin");
     alert(param);
     param=decodeURIComponent(param);
     alert(param);
     document.getElementById("index-pin").value=param;
+    document.getElementById("index-share").onclick=function () {
+        var pin=document.getElementById("index-pin").value;
+        var url="https://cuixingchen.github.io/FbShare_LineShare/fb/";
+        url+="?pin="+encodeURIComponent(pin);
+        facebookSDK.share(url,function (response) {
+            if (response && !response.error_message) {
+                alert('Posting completed.');
+            } else {
+                alert('Error while posting.');
+            }
+        });
+    }
 };
 var pageObj = {
     "getParameter": function (param) {
